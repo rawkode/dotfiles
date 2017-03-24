99912f4daa732b546b6ba0963a0e7cb8efcad155
David McKay <david@rawkode.com>
2017-03-24 21:58:29 +0100

    Adding magit to emacs
    Signed-off-by: David McKay <david@rawkode.com>

--- states/agnostic/emacs/custom.el
+++ states/agnostic/emacs/custom.el
@@ -1,38 +1,19 @@
 (custom-set-variables
-  '(custom-safe-themes (quote("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
-  '(helm-gtags-prefix-key "")
-  '(helm-gtags-suggested-key-mapping t)
-  '(package-selected-packages
-    (quote
-      (linum-relative
-        rainbow-delimiters
-        rainbow-mode
-        smart-mode-line
-        smartparens
-        expand-region
-        php-mode
-        ace-jump-mode
-        salt-mode
-        dockerfile-mode
-        docker
-        helm-ls-git
-        git-gutter
-        org
-        helm-etags-plus
-        helm-github-stars
-        helm-gitlab
-        helm-gtags
-        helm-projectile
-        general
-        base16
-        evil-surround
-        evil-leader
-        evil-mode
-        helm
-        evil-visual-mark-mode
-        neotree
-      )
-    )
-  )
-)
-(custom-set-faces)
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(helm-gtags-prefix-key "")
+ '(helm-gtags-suggested-key-mapping t)
+ '(package-selected-packages
+   (quote
+    (magit linum-relative rainbow-delimiters rainbow-mode smart-mode-line smartparens expand-region php-mode ace-jump-mode salt-mode dockerfile-mode docker helm-ls-git git-gutter org helm-etags-plus helm-github-stars helm-gitlab helm-gtags helm-projectile general base16 evil-surround evil-leader evil-mode helm evil-visual-mark-mode neotree))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
