a0aa70110f8ea721d3382ce0a974d5a73901afd0
David McKay <david.andrew.mckay@gmail.com>
2017-09-05 16:40:24 +0200

    Simplfying gpg / fish

--- states/agnostic/fish/config.fish
+++ states/agnostic/fish/config.fish
@@ -2,30 +2,9 @@ if status --is-interactive
   source $HOME/.config/base16-shell/profile_helper.fish
 end
 
-# GPG agent
-function __refresh_gpg_agent_info -d "Reload ~/.gpg-agent-info into environment"
-	cat ~/.gpg-agent-info | sed 's/=/ /' | while read key value
-		set -e $key
-		set -U -x $key "$value"
-	end
-end
+# Set GPG TTY
+set -x GPG_TTY (tty)
+set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/.gnupg/S.gpg-agent.ssh
 
-if command -s gpg-agent > /dev/null
-	if not set -q -x GPG_AGENT_INFO
-		gpg-agent --daemon --write-env-file ~/.gpg-agent-info > /dev/null ^ /dev/null
-	end
-
-	if test -f ~/.gpg-agent-info
-		__refresh_gpg_agent_info
-
-		gpg-connect-agent /bye ^/dev/null
-		if test $status -eq 1
-			pkill -U $USER gpg-agent
-			gpg-agent --daemon --write-env-file ~/.gpg-agent-info > /dev/null ^ /dev/null
-			__refresh_gpg_agent_info
-		end
-	end
-
-	# Use curses based Pinentry
-	set -x GPG_TTY (tty)
-end
+gpg-connect-agent killagent /bye
+gpg-connect-agent /bye
