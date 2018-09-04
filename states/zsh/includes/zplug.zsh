e5d9bd2e359844347dbdff3e07785eac9c02c0c1
David McKay <david.andrew.mckay@gmail.com>
2018-09-04 19:25:33 +0100

    Switching Back to OceanicNext

--- states/zsh/includes/zplug.zsh
+++ states/zsh/includes/zplug.zsh
@@ -1,6 +1,6 @@
 # Favourites
 # bespin, brewer, bright, chalk, dracula, eighties, gnome-dark, gruvbox-dark, monokao, nord, seti, tomorrow-night
-zplug "chriskempson/base16-shell", use:"scripts/base16-tomorrow-night.sh"
+zplug "chriskempson/base16-shell", use:"scripts/base16-oceanicnext.sh"
 
 ## Can't decide if these are useful or annoying
 zplug "rawkode/zsh-docker-run"
