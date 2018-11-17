24ae5b1017fc90a8ff6f9dc16462d98237d40f67
David McKay <david.andrew.mckay@gmail.com>
2018-11-17 15:53:15 +0000

    Android Path

--- states/zsh/includes/paths.zsh
+++ states/zsh/includes/paths.zsh
@@ -1,7 +1,8 @@
 export GOPATH=/code
 
+export ANDROID_HOME=/opt/android-sdk
 export CARGO_PATH=$HOME/.cargo/bin
 export COMPOSER_PATH=$HOME/.composer/vendor/bin
 export FLUTTERPATH=/opt/flutter/bin
 
-export PATH=$PATH:$CARGO_PATH:$COMPOSER_PATH:$FLUTTERPATH:$GOPATH/bin
+export PATH=$PATH:$CARGO_PATH:$COMPOSER_PATH:$FLUTTERPATH:$GOPATH/bin:$ANDROID_HOME/tools/bin
