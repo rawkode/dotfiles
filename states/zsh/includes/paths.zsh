465ce60df933381caea376b68ea489a934c05658
David McKay <david@rawkode.dev>
2019-04-09 20:55:35 +0100

    Clean Up Paths

--- states/zsh/includes/paths.zsh
+++ states/zsh/includes/paths.zsh
@@ -1,8 +1,18 @@
+export ANDROID_HOME=/opt/android-sdk
+export COMPOSER_HOME=$HOME/.composer
 export GOPATH=$HOME/Code
 
-export ANDROID_HOME=/opt/android-sdk
-export CARGO_PATH=$HOME/.cargo/bin
-export COMPOSER_PATH=$HOME/.composer/vendor/bin
-export FLUTTERPATH=/opt/flutter/bin
+# In zsh, the $PATH variable is tied to the $path variable.
+# This makes the $path variable act like a set.
+typeset -U path
 
-export PATH=$PATH:$CARGO_PATH:$COMPOSER_PATH:$FLUTTERPATH:$GOPATH/bin:$ANDROID_HOME/tools/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+path=("$ANDROID_HOME/tools/bin")
+path=("$ANDROID_HOME/tools")
+path=("$ANDROID_HOME/platform-tools")
+path=("$COMPOSER_HOME/vendor/bin")
+path=("$GOPATH/bin" $path)
+path=("$HOME/.cargo/bin" $path)
+path=("/opt/flutter/bin")
+
+# Using the (N-/) glob qualifier we can remove paths that do not exist.
+path=($^path(N-/))
