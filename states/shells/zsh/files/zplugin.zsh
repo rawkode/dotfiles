c242b23dabeddd82d40360ec0fb732830beceedd
David McKay <david@rawkode.dev>
2019-10-14 12:51:00 +0100

    chore: switch to base16 dracula

--- states/shells/zsh/files/zplugin.zsh
+++ states/shells/zsh/files/zplugin.zsh
@@ -4,7 +4,7 @@ zplugin load "chrissicool/zsh-256color"
 ##
 # Base16
 ##
-zplugin ice pick"async.sh" src"scripts/base16-ocean.sh"
+zplugin ice pick"async.sh" src"scripts/base16-dracula.sh"
 zplugin load "chriskempson/base16-shell"
 
 ##
