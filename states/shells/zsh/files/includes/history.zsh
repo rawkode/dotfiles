28a1876cd158ea138c87b6755f1e60bb82a5a481
David McKay <david@rawkode.dev>
2019-10-16 12:18:11 +0100

    chore: silent loaded messages

--- states/shells/zsh/files/includes/history.zsh
+++ states/shells/zsh/files/includes/history.zsh
@@ -1,11 +1,11 @@
 ##
 # Plugins
 ##
-zplugin ice wait"0"
+zplugin ice silent wait"0"
 zplugin snippet OMZ::plugins/history/history.plugin.zsh
-zplugin ice wait"0"
+zplugin ice silent wait"0"
 zplugin snippet OMZ::plugins/per-directory-history/per-directory-history.zsh
-zplugin ice wait"0"
+zplugin ice silent wait"0"
 zplugin light zdharma/history-search-multi-word
 zplugin ice silent wait"0" pick"zsh-history-substring-search.zsh"
 zplugin load "zsh-users/zsh-history-substring-search"
