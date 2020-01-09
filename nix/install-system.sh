0efb9d71064fe17884a4b46a9afed85c4bdc1ca1
David McKay <rawkode@pm.me>
2020-01-09 11:34:51 +0000

    chore: make system installation a little nicer by guessing which machine

--- nix/install-system.sh
+++ nix/install-system.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env sh
-echo -e "Which machine?"
+sys_machine=$(cat /sys/class/dmi/id/product_version)
+guess_machine=''
+
+if [[ $sys_machine =~ "ThinkPad X1 Carbon" ]]
+then
+  guess_machine='x1-carbon'
+fi
+
+echo -e "Which machine? Press enter if it's ${guess_machine} ... "
 read -p "> " machine
-echo -e "Press <enter> to provision '${machine}'?\n  (Control-C to cancel)"
-read -p "> "
+actual_machine=${machine:-${guess_machine}}
+
+# Different machine, ask for confirmation
+if [[ $actual_machine != $guess_machine ]]
+then
+  echo -e "Press <enter> to confirm provision '${actual_machine}'?\n  (Control-C to cancel)"
+  read -p "> "
+fi
 
 set -e
 
-sudo cp ./machines/${machine}/configuration.nix /etc/nixos/
+sudo cp ./machines/${actual_machine}/configuration.nix /etc/nixos/
 
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 sudo nix-channel --update
