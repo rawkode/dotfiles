8e689bfdd3311d781b5df70b6725296ead27357a
David McKay <rawkode@pm.me>
2020-01-09 10:45:05 +0000

    fix: move hub completions ice

--- dotfiles/zsh/includes/git.zsh
+++ dotfiles/zsh/includes/git.zsh
@@ -1,10 +1,4 @@
-##
-# Will uncomment when I get the auto-complete for git and hub working
-#
-#alias git='hub'
+alias git='hub'
 
-zplugin ice wait lucid as"completion"
-zplugin snippet https://git.kernel.org/pub/scm/git/git.git/plain/contrib/completion/git-completion.zsh
-
-zplugin ice wait lucid as"completion"
+zplugin ice wait lucid as"completion" mv'*ion -> _hub'
 zplugin snippet https://github.com/github/hub/raw/master/etc/hub.zsh_completion
